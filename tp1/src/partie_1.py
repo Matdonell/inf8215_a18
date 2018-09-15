d83f265c3828fec8de4656e0662f4b39a7310ba4
Reph Dauphin Mombrun <remom@l4714-21.info.polymtl.ca>
2018-09-15 13:09:42 -0400

    Implementation of bfs algo // Reph

--- tp1/src/partie_1.py
+++ tp1/src/partie_1.py
@@ -11,10 +11,10 @@ import time
 import math
 
 
-try:
-    import Queue as q
-except ImportError:
-    import q
+# try:
+#     import Queue as q
+# except ImportError:
+#     import q
 
 class Solution:
     def __init__(self, places, graph):
@@ -28,15 +28,14 @@ class Solution:
         self.visited = [places[0]]  # list of already visited attractions
         # list of attractions not yet visited
         self.not_visited = copy.deepcopy(places[1:])
+        self.places = places
 
     def add(self, idx):
-        s=Solution(places, graph)
-        s.visited=self.visited
-        s.not_visited=self.not_visited
-        s.g = self.g
-        s.g +=graph[s.visited[-1]][self.not_visited[idx]]
+        s= copy.deepcopy(self)
+       # print("id ", idx, "--", len(self.not_visited))
+        s.g +=self.graph[s.visited[-1]][self.not_visited[idx]]
         s.visited.append(self.not_visited[idx])
-        del self.not_visited[idx]
+        del s.not_visited[idx]
         return s
        
         """
@@ -49,31 +48,33 @@ def bfs(graph, places):
     """
     Returns the best solution which spans over all attractions indicated in 'places'
     """
-    racine=Solution(places, graph) 'premier noeud'
-    q =[] 'création de la file'
-    q.append(racine) 'file initiale'
-    while len(q[0].not_visited) >= 2: 'tant que il reste des points non visités dans la première solution de la file'
-         solution_mere=q[0]; 'on prend le premier de la file'
-         for i in range(len(solution_mere.not_visited)-1): 'on construit ses enfants'
-             q.append(add(solution_mere, i)
-        del q[0] 'on supprime le premier de la file'
-        min=math.inf
-        optimum=q[0]
-    for j in range(len(q)):
-        q[j]=add(q[j], 0) 'ajout de pm'
-        if q[j].g<=min:
-            min=q[j].g
-            optimum=q[j]
-    return min
-    
-    
-    
-    
-    
-        
-    
-    
-    return Solution(places, graph)
-    
-    
-    
+    racine=Solution(places, graph) # premier noeud'
+    q0 = [racine]  # création de la file initiale
+
+    while len(q0[0].not_visited) >= 2: # tant que il reste des points non visités dans la première solution de la file'
+         solution_mere=q0[0] # on prend le premier de la file'
+
+         for i in range(len(solution_mere.not_visited)-1): # on construit ses enfants'
+             #print("--", i)
+             q0.append(solution_mere.add(i))
+
+             #print ("before", q0)
+
+         del q0[0] # on supprime le premier de la file'
+        # print ("after", q0)
+
+
+    min=math.inf
+    optimum=q0[0]
+    q1=[]
+
+    for j in range(len(q0)):
+        q1.append(q0[j].add(0))
+       # print ("before2", q1) # ajout de pm'
+
+        if q1[j].g<=min:
+            min=q1[j].g
+            optimum=q1[j]
+           # print(min)
+
+    return optimum
