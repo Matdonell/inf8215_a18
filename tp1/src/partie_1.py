b15980ba1d390ae554d98d646f3b7a33ed26a616
FloGab <FloGab@github.com>
2018-09-15 11:07:31 -0400

    partie1

--- tp1/src/partie_1.py
+++ tp1/src/partie_1.py
@@ -28,15 +28,31 @@ class Solution:
         self.not_visited = copy.deepcopy(places[1:])
 
     def add(self, idx):
+        s=Solution(places, graph)
+        s.visited=self.visited
+        s.not_visited=self.not_visited
+        s.g = self.g
+        s.g +=graph[s.visited[-1]][self.not_visited[idx]]
+        s.visited.append(self.not_visited[idx])
+        del self.not_visited[idx]
+        return s
+       
         """
         Adds the point in position idx of not_visited list to the solution
         """
-        # TODO : to implement
 
 
 def bfs(graph, places):
+    
     """
     Returns the best solution which spans over all attractions indicated in 'places'
     """
-    # TODO : to implement
+    s0=Solution(places, graph)
+    q = Queue()
+    q.push(s0)
+    
+    
     return Solution(places, graph)
+    
+    
+    
