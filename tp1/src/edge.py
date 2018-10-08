696a3404f57a1c91dff28ab3a12a4a81f9267a48
Mathieu KABORE <tanga-mathieu.kabore@polymtl.ca>
2018-10-08 12:41:41 -0400

    Add an edge representation class

---
+++ tp1/src/edge.py
@@ -0,0 +1,16 @@
+class Edge:
+    def __init__(self, from_v, to_v, weight):
+        """
+        from_v : source of the edge
+        to_v: end of the edge
+        weight: related weight of the edge
+        """
+        self.from_v = from_v
+        self.to_v = to_v
+        self.weight = weight
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def print(self):
+        print(self.from_v,'-->', self.to_v, '(', self.weight, ')')
