c13965b18a7fab6ce39bf4622fca2ace17b4d80c
Mathieu KABORE <tanga-mathieu.kabore@polymtl.ca>
2018-10-08 12:42:20 -0400

    Add v1 draft of Edmond MST algo
    Add draft v2

--- tp1/src/partie_2.py
+++ tp1/src/partie_2.py
@@ -9,6 +9,8 @@ import copy
 import heapq
 import math
 
+from edge import Edge
+
 # Ensure to support Python 2 and 3 by using the correct import
 try:
     import queue as q
@@ -74,7 +76,7 @@ def fastest_path_estimation(sol):
     for i in range(len(sol.not_visited)):
         sub_node = copy.deepcopy(sol)
         sub_node.add(i)
-        sub_node.h = math.inf
+        sub_node.g = math.inf
 
         # init all distances with infinite value for the sub nodes
         distance_map[sol.not_visited[i]] = math.inf
@@ -90,7 +92,7 @@ def fastest_path_estimation(sol):
 
         # If the last visited is the destination, we are done
         if best_sol.visited[-1] == pm:
-            return best_sol.h
+            return best_sol.g
 
         # For each unvisited sub node of the current node
         for i in range(len(best_sol.not_visited)):
@@ -105,7 +107,7 @@ def fastest_path_estimation(sol):
 
                 sub_node = copy.deepcopy(best_sol)
                 sub_node.add(i)
-                sub_node.h = new_cost
+                sub_node.g = new_cost
                 heapq.heappush(T, sub_node)
 
     return None
@@ -125,32 +127,171 @@ def A_star(graph, places):
     heapq.heappush(T, root)
     best_solution = copy.deepcopy(root)
 
-    while best_solution.not_visited:
-        best_solution = heapq.heappop(T)
+    edges = []
+    heapq.heapify(edges)
 
-        # Since we are skipping the destination node pm when expanding the sub nodes
-        # if we have one last node to visit for a specific branch (pm)
-        # Add it into the solution and stop the search
-        if len(best_solution.not_visited) == 1:
-            best_solution.add(0)
-            return best_solution
-        else:
-            # Generate the sub nodes (a.k.a expand the sub solutions) from the current
-            # Notice: skip the destination attraction for now
-            # And find the fastest path between the node and the destination
-            for i in range(len(best_solution.not_visited) - 1):
-                new_sol = copy.deepcopy(best_solution)
-                new_sol.add(i)
+    # Generate all the outgoing edges from the root
+    # for i in range(len(root.not_visited)):
+    #     weight = root.graph[root.visited[-1]][root.not_visited[i]]
+    #     edge = Edge(root.visited[-1], root.not_visited[i], weight)
+    #     heapq.heappush(edges, edge)
 
-                # Update the fastest path to pm
-                new_sol.h = fastest_path_estimation(new_sol)
-                heapq.heappush(T, new_sol)
+    # Generate all edges between the nodes except the root
+    for i in range(len(root.not_visited)):
+        for j in range(len(root.not_visited)):
+
+            # Avoid creating an edge between the same node (self-cycle)
+            if root.not_visited[i] != root.not_visited[j]:
+                # Get the weight
+                weight = root.graph[root.not_visited[i]][root.not_visited[j]]
+
+                # Create the node and add it to the queue
+                edge = Edge(root.not_visited[i], root.not_visited[j], weight)
+
+                heapq.heappush(edges, edge)
+
+    for edge in edges:
+        edge.print()
+
+    # new_sol = minimum_spanning_arborescence(root.not_visited, edges, root.visited[-1])
+
+    # print("total_cost", get_total_cost(new_sol))
+    #
+    # while best_solution.not_visited:
+    #     best_solution = heapq.heappop(T)
+    #
+    #     # Since we are skipping the destination node pm when expanding the sub nodes
+    #     # if we have one last node to visit for a specific branch (pm)
+    #     # Add it into the solution and stop the search
+    #     if len(best_solution.not_visited) == 1:
+    #         best_solution.add(0)
+    #         return best_solution
+    #     else:
+    #         # Generate the sub nodes (a.k.a expand the sub solutions) from the current
+    #         # Notice: skip the destination attraction for now
+    #         # And find the fastest path between the node and the destination
+    #         for i in range(len(best_solution.not_visited) - 1):
+    #             new_sol = copy.deepcopy(best_solution)
+    #             new_sol.add(i)
+    #
+    #             # Update the fastest path to pm
+    #             # new_sol.h = fastest_path_estimation(new_sol)
+    #             new_sol.h = get_total_cost(minimum_spanning_arborescence(root.not_visited, edges, root.visited[-1]))
+    #             heapq.heappush(T, new_sol)
 
     return best_solution
 
 
-def minimum_spanning_arborescence(sol):
+def minimum_spanning_arborescence(V, E, root):
     """
     Returns the cost to reach the vertices in the unvisited list
     """
-    # TODO : to implement
+    # if not isinstance(sol, Solution):
+    #     raise ValueError("The parameter should be a type of Solution")
+
+    # root = sol.visited[-1]  # The root is the last visited node
+    # pm = sol.not_visited[-1]  # the destination node
+
+    new_node_value = len(V)
+
+    best_in_edge = {}
+    kicks_out = {}
+    real = {}
+    E = copy.deepcopy(E)
+
+    for v in V:
+        incoming_edge_to_v = []
+        heapq.heapify(incoming_edge_to_v)
+
+        for tmp in E:
+            if tmp.to_v == v:
+                heapq.heappush(incoming_edge_to_v, tmp)
+        best_in_edge[v] = heapq.heappop(incoming_edge_to_v)
+
+        for e in best_in_edge:
+            cycle = get_cycle(e, copy.deepcopy(E))
+            if cycle is not None:
+                break
+
+        if cycle is None:
+            return best_in_edge
+
+        # build a new graph in which C is contracted into a single node
+        v_C = new_node_value  # vC ← new Node
+        V_0 = [n for n in V if not [c for c in cycle if c.from_v == n or c.to_v == n]]  # V0 ← V ∪ {vC } \ C
+        V_0.append(v_C)
+        E_0 = []
+
+        for e in E:  # e = (t, u) in E:
+            contains_t = [c for c in cycle if c.from_v == e.from_v or c.to_v == e.from_v]
+            contains_u = [c for c in cycle if c.from_v == e.to_v or c.to_v == e.to_v]
+
+            if not contains_t and not contains_u:  # if t not ∈ C and u not∈ C:
+                e_0 = e  # e0 ← e
+
+            elif contains_t and not contains_u:
+                e_0 = Edge(v_C, e.to_v, e.weight)  # e0 ← new Edge(vC, u)
+                # score[e0] ← score[e]
+
+            elif contains_u and not contains_t:
+                # e0 ← new Edge(t, vC)
+                e_0 = Edge(e.from_v, v_C, 0)
+
+                # kicksOut[e0] ← bestInEdge[u]
+                kicks_out[(e_0.from_v, e_0.to_v)] = best_in_edge[e.to_v]
+
+                # score[e0] ← score[e] − score[kicksOut[e0].weight]
+                e_0.weight = e.weight - kicks_out[(e_0.from_v, e_0.to_v)].weight
+
+            real[e_0] = e  # remember the original
+            E_0.append(e_0)  # E0 ← E0 ∪ {e0}
+
+        A = minimum_spanning_arborescence(V_0, E_0, root)  # A ← Get1Best({V0, E0}, ROOT )
+
+        path = []
+        if len(A) >= v_C:
+            for e in cycle:
+                if not [k for k in kicks_out[A[v_C]] if e.from_v != k.from_v and e.to_v != k.to_v]:
+                    path.append(e)
+
+        path.append(real[e_0])
+        return path  # return {real[e0] | e0 ∈ A} ∪ (CE \ {kicksOut[A[vC]]})
+
+    return best_in_edge
+
+
+def get_cycle(node, edges, cycle=None):
+    """ best_in_edge : list of edge """
+
+    temp = [e for e in edges if e.from_v == node]
+
+    if not temp:
+        return cycle
+
+    edge = temp[0]
+
+    if cycle is None:
+        cycle = []
+
+    if edge is not None:
+        cycle.append(edge)
+        if edge.to_v == cycle[0].from_v:
+            for c in cycle:
+                c.print()
+            return cycle
+        else:
+            edges.remove(edge)
+            cycle = get_cycle(edge.to_v, edges, cycle)
+
+    for c in cycle:
+        c.print()
+
+    return cycle
+
+
+def get_total_cost(list_of_edges):
+    cost = 0
+    for edge in list_of_edges:
+        cost += edge.weight
+
+    return cost
