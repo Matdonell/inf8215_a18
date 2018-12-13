211cb2658e2e184ba32b0f2a2c7d1bc1f1fe9990
Mathieu KABORE <tanga-mathieu.kabore@polymtl.ca>
2018-12-12 22:13:49 -0500

    update softmax classifier test file

--- tp3/SoftmaxClassifierTests.py
+++ tp3/SoftmaxClassifierTests.py
@@ -10,28 +10,45 @@ import time
 import numpy as np
 from SoftmaxClassifier import SoftmaxClassifier
 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import precision_recall_fscore_support
+import matplotlib.pyplot as plt
+
 
 class SoftmaxClassifierTests(unittest.TestCase):
     def __init__(self, method_name='runTest'):
         super(SoftmaxClassifierTests, self).__init__(method_name)
 
     def setUp(self):
-        self.softmaxClassifier = SoftmaxClassifier()
+        # import the custom classifier
+        self.softmax_classifier = SoftmaxClassifier()
 
-    def test_onehot_encoder(self):
-        # Arrange
-        y = [1, 1, 2, 3, 1]
+    def test_softmax_classifier(self):
+        # load dataset
+        data, target = load_iris().data, load_iris().target
 
-        expected = np.array([[1, 0, 0],
-                    [1, 0, 0],
-                    [0, 1, 0],
-                    [0, 0, 1],
-                    [1, 0, 0]]).tolist()
-        # Act
-        yohe = self.softmaxClassifier._one_hot(y)
+        # split data in train/test sets
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
 
-        # Assert
-        self.assertEqual(expected, yohe)
+        # standardize columns using normal distribution
+        # fit on X_train and not on X_test to avoid Data Leakage
+        s = StandardScaler()
+        X_train = s.fit_transform(X_train)
+        X_test = s.transform(X_test)
+
+        # train on X_train and not on X_test to avoid overfitting
+        train_p = self.softmax_classifier.fit_predict(X_train, y_train)
+        test_p = self.softmax_classifier.predict(X_test)
+
+        # display precision, recall and f1-score on train/test set
+        print("train : " + str(precision_recall_fscore_support(y_train, train_p, average="macro")))
+        print("test : " + str(precision_recall_fscore_support(y_test, test_p, average="macro")))
+
+        plt.plot(self.softmax_classifier.losses_)
+        plt.show()
+
 
 if __name__ == '__main__':
     unittest.main()
